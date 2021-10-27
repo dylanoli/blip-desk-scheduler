9b2f730102af2a78f654bfc2e39f6706631e3671
Dylan <dylan.oli@hotmail.com>
2021-10-27 19:08:44 -0300

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,71 +1,156 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useTranslation, Trans } from 'react-i18next';
-import RoutesPath from '../../constants/routes-path';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+// eslint-disable-next-line import/named
+import { withLoadingAsync } from '../../services/common-service';
+import { getApplicationDataAsync } from '../../services/application-service';
+import {
+    getResourceAsync,
+    saveResourceAsync
+} from '../../services/resources-service';
 
 import settings from '../../config';
 
-import logo from '../../assets/images/svg/blip-balloon.svg';
 import Header from './components/Header';
+import Button from '../../components/Button';
+import { DEFAULT_TIME } from './constants';
+import './index.css';
+import DayOff from './components/DaysOff';
+import ListWeek from './components/ListWeek';
 
 const PAGE_ICON = 'plugin';
 const BLANK = '_blank';
+const WORK_TIME_NAME = 'workTime';
 
 const Home = () => {
-    const history = useHistory();
+    const [times, setTimes] = useState(null);
+    const [application, setApplication] = useState({});
     const { t } = useTranslation();
 
-    const handleNavigation = useCallback(
-        (path, params = {}) => {
-            history.push(path, params);
-        },
-        [history]
-    );
+    useEffect(() => {
+        withLoadingAsync(async () => {
+            setApplication(await getApplicationDataAsync());
 
+            const resourceTimes = JSON.parse(
+                await getResourceAsync(WORK_TIME_NAME)
+            );
+            if (resourceTimes.weekdays && resourceTimes.noWorkDays) {
+                setTimes(resourceTimes);
+            } else {
+                setTimes(DEFAULT_TIME);
+            }
+        });
+    }, [application.shortName]);
+
+    const saveAsync = async () => {
+        await saveResourceAsync(WORK_TIME_NAME, times);
+    };
+
+    const removeDayOff = (index) => {
+        const newVal = { ...times };
+        newVal.noWorkDays.splice(index, 1);
+        setTimes(newVal);
+    };
+
+    const removeWorkTime = (indexWeek, indexHour) => {
+        const newVal = { ...times };
+        newVal.weekdays[indexWeek].workTimes.splice(indexHour, 1);
+        setTimes(newVal);
+    };
+
+    const changeStart = (indexWeek, indexHour, event) => {
+        const newVal = { ...times };
+        const weekdays = newVal.weekdays[indexWeek];
+        const workTime = weekdays.workTimes.find(
+            (_item, index) => index === indexHour
+        );
+        if (workTime) {
+            workTime.start = event.target.value;
+        }
+        setTimes(newVal);
+    };
+
+    const changeEnd = (indexWeek, indexHour, event) => {
+        const newVal = { ...times };
+        const weekdays = newVal.weekdays[indexWeek];
+        const workTime = weekdays.workTimes.find(
+            (_item, index) => index === indexHour
+        );
+        if (workTime) {
+            workTime.end = event.target.value;
+        }
+        setTimes(newVal);
+    };
+
+    const changeDayOff = (index, event) => {
+        const newVal = { ...times };
+        newVal.noWorkDays[index] = event.target.value;
+        setTimes(newVal);
+    };
+
+    const addWorkTime = (index) => {
+        const newItem = {
+            start: '09:00',
+            end: '19:00'
+        };
+
+        const newVal = { ...times };
+        if (newVal.weekdays[index].workTimes) {
+            newVal.weekdays[index].workTimes.push(newItem);
+        } else {
+            newVal.weekdays[index].workTimes = [newItem];
+        }
+        setTimes(newVal);
+    };
+
+    const addDayOff = () => {
+        const newItem = 'MM-DD';
+        const newVal = { ...times };
+        newVal.noWorkDays.push(newItem);
+        setTimes(newVal);
+    };
+
+    if (times != null) {
+        return (
+            <div className="ph1 ph4-m ph5-ns pb5">
+                <Header
+                    title={t('title.homePage')}
+                    icon={PAGE_ICON}
+                    onClick={() => window.open(settings.repositoryUrl, BLANK)}
+                />
+                <h2>Dias de trabalho</h2>
+                <div className="week-container">
+                    <ListWeek
+                        times={times}
+                        changeStart={changeStart}
+                        changeEnd={changeEnd}
+                        removeWorkTime={removeWorkTime}
+                        addWorkTime={addWorkTime}
+                    />
+                </div>
+                <h2>Dias sem trabalhos</h2>
+                <DayOff
+                    noWorkDays={times.noWorkDays}
+                    changeDayOff={changeDayOff}
+                    removeDayOff={removeDayOff}
+                    addDayOff={addDayOff}
+                />
+                {JSON.stringify(times)}
+                <br />
+                <br />
+                <Button
+                    text={t('labels.save')}
+                    icon="save-disk"
+                    variant="primary"
+                    arrow={false}
+                    disabled={false}
+                    onClick={saveAsync}
+                />
+            </div>
+        );
+    }
     return (
         <div className="ph1 ph4-m ph5-ns pb5">
-            <Header
-                title={t('title.homePage')}
-                icon={PAGE_ICON}
-                onClick={() => window.open(settings.repositoryUrl, BLANK)}
-            />
-            <div className="flex flex-column items-center justify-center bp-c-neutral-dark-city f5 h-100 mt4">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p className="tc">
-                    {t('paragraph.homeDescription.part1')}
-                    <br />
-                    <Trans i18nKey="paragraph.homeDescription.part2">
-                        Edit <code>src/pages/Home.js</code> and save to reload.
-                    </Trans>
-                </p>
-                <h5 className="f5 b mt3 mb2">{t('title.exemples')}</h5>
-                <a
-                    className="f6 flex items-center blue no-underline underline-hover"
-                    href="#0"
-                    data-testid="exemple-one"
-                    onClick={() =>
-                        handleNavigation(RoutesPath.EXAMPLE_PAGE.PATH, {
-                            type: 'storedData'
-                        })
-                    }
-                >
-                    <bds-icon name="file-txt-1" size="x-small" />
-                    {t('link.storedExemple')}
-                </a>
-                <a
-                    className="f6 flex items-center blue no-underline underline-hover mt1"
-                    href="#0"
-                    data-testid="exemple-two"
-                    onClick={() =>
-                        handleNavigation(RoutesPath.EXAMPLE_PAGE.PATH, {
-                            type: 'swrCall'
-                        })
-                    }
-                >
-                    <bds-icon name="file-txt-1" size="x-small" />
-                    {t('link.swrExemple')}
-                </a>
-            </div>
+            <h2>{t('loading')}</h2>
         </div>
     );
 };
